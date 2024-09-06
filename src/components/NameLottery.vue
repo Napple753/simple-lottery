9d77de68058bd134776b5703e017238feada4375
Napple753 <napple.com@gmail.com>
2024-09-06 22:18:21 +0900

    fix: style for multiple prizes
    Refs: #63

--- src/components/NameLottery.vue
+++ src/components/NameLottery.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import { ref, Ref } from "vue";
+import { ref, Ref, computed } from "vue";
 import { Candidate, DisplaySetting } from "@/myTypes";
 import ConfirmRedraw from "./ConfirmRedraw.vue";
 import NameLotteryCore from "./NameLotteryCore.vue";
 import MarkedText from "./MarkedText.vue";
+import { useMarkdownStore } from "@/store/markdown";
+const markdownStore = useMarkdownStore();
 const emit = defineEmits<{
   (e: "finishDraw"): void;
   (e: "redraw", winner: Candidate): void;
@@ -52,32 +54,44 @@ function redraw() {
 }
 
 defineExpose({ draw });
+
+const prizeNameFontWeight = computed(() =>
+  markdownStore.markdown ? "normal" : "bold",
+);
+const wrapperMaxWidth = computed(() => (props.subPrizeName ? "100%" : "30rem"));
 </script>
 
 <template>
-  <div style="max-width: 30rem">
-    <MarkedText :markdown="subPrizeName" v-if="subPrizeName"></MarkedText>
-    <NameLotteryCore
-      ref="lotteryCore"
-      :winner="winner"
-      :candidates="candidates"
-      :isSimple="isSimple"
-      :displaySetting="displaySetting"
-      :immediate="immediate"
-      :playDrumRoll="playDrumRoll"
-      @finishDraw="finishDraw"
-    ></NameLotteryCore>
-    <div style="display: flex; justify-content: end; height: 48px">
-      <v-btn
-        icon
-        class="reload"
-        v-show="isDeciding && !noRedraw"
-        flat
-        @click="redrawDialog = true"
-      >
-        <v-icon size="small" icon="mdi-reload"></v-icon>
-      </v-btn>
+  <div class="nameRotteryWrapper">
+    <MarkedText
+      :markdown="subPrizeName"
+      v-if="subPrizeName"
+      class="subPrizeName"
+    ></MarkedText>
+    <div style="max-width: 30rem; width: 100%">
+      <NameLotteryCore
+        ref="lotteryCore"
+        :winner="winner"
+        :candidates="candidates"
+        :isSimple="isSimple"
+        :displaySetting="displaySetting"
+        :immediate="immediate"
+        :playDrumRoll="playDrumRoll"
+        @finishDraw="finishDraw"
+      ></NameLotteryCore>
+      <div style="display: flex; justify-content: end; height: 48px">
+        <v-btn
+          icon
+          class="reload"
+          v-show="isDeciding && !noRedraw"
+          flat
+          @click="redrawDialog = true"
+        >
+          <v-icon size="small" icon="mdi-reload"></v-icon>
+        </v-btn>
+      </div>
     </div>
+
     <ConfirmRedraw
       v-model="redrawDialog"
       :winner="winner"
@@ -88,4 +102,21 @@ defineExpose({ draw });
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.nameRotteryWrapper {
+  max-width: v-bind(wrapperMaxWidth);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.subPrizeName {
+  font-size: 1em;
+  font-weight: v-bind(prizeNameFontWeight);
+  text-align: center;
+  display: flex;
+  height: 10rem;
+  flex-direction: column;
+  align-items: center;
+  justify-content: end;
+}
+</style>
